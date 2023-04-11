27146f387f2bec3248b24f64c55a647c8f05d7f5
Fábio Trindade <fabiotrindadefer@gmail.com>
2023-04-11 13:20:34 -0300

    Add(+): Versão 2.0.2 lançada no PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setup(
     name='Biblioteca RIT',
-    version='2.0.0',
+    version='2.0.2',
     url='https://github.com/BibliotecaRIT/BibliotecaRIT',
     license='MIT License',
     author='NuPESSC',
