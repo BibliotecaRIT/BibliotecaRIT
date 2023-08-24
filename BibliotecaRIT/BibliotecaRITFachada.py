bdc4a603f4bf8027af392d95a93b315cd3519eb5
Fábio Trindade <fabiotrindadefer@gmail.com>
2023-08-24 11:44:00 -0300

    Mod(-): Refatoração BibliotecaRITFachada

--- BibliotecaRIT/BibliotecaRITFachada.py
+++ BibliotecaRIT/BibliotecaRITFachada.py
@@ -1,7 +1,10 @@
+from copy import deepcopy
 from BibliotecaRIT.Sources.Requisicao import Requisicao
 from BibliotecaRIT.Sources.controladoras.ControladoraCalculoRelevancia import ControladoraCalculoRelevancia
 from BibliotecaRIT.Sources.controladoras.ControladoraExportacaoDados import ControladoraExportacaoDados
 from BibliotecaRIT.Sources.controladoras.ControladoraExtracaoDados import ControladoraExtracaoDados
+from BibliotecaRIT.Sources.controladoras.ControladoraPreProcessamento import ControladoraPreProcessamento
+from BibliotecaRIT.Sources.entidades.Projeto import Projeto
 from BibliotecaRIT.Sources.estrategias.exportacao.VisaoRelevanciaTematicaPorData import VisaoRelevanciaTematicaPorData
 from BibliotecaRIT.Sources.estrategias.exportacao.VisaoRelevanciaTematicaPorAutor import VisaoRelevanciaTematicaPorAutor
 
@@ -18,30 +21,51 @@ class BibliotecaRITFachada:
         for i in range(pagInicial,qtdPaginas+1):
             print(f'-- Extraindo os dados da página {i}/{qtdPaginas} do repositório {repositorio} --')
             projeto = ControladoraExtracaoDados.requisicaoIssuesPorPagina(usuario,repositorio,i)
+            
+            print(f'-- Pré-processando os dados da página {i}/{qtdPaginas} do repositório {repositorio} --')
+            projetoPreProcessado = cls.__preProcessarProjeto(projeto)
 
             print(f'-- Calculando a Relevância Temática dos Comentários da página {i}/{qtdPaginas} do repositório {repositorio} --')
-            cls.__calcularRelevanciaTematicaComentariosGitHub(projeto)
+            cls.__calcularRelevanciaTematicaComentariosGitHub(projeto,projetoPreProcessado)
      
             print(f'-- Exportando dados da página {i}/{qtdPaginas} do repositório {repositorio} --')
             cls.__gerarCSVGitHub(projeto, visao, i, arg)
+            
             print('\033[92m' + f'Página {i}/{qtdPaginas} do repositório {repositorio} finalizada' + '\033[0m')
             print()
 
     @staticmethod
-    def __calcularRelevanciaTematicaComentariosGitHub(projeto):
-        comentariosAnteriores = ''
-        for topico in projeto.topicos:
-            for comentario in topico.listaComentarios:
-                relevancia = ControladoraCalculoRelevancia.relevanciaTematica(comentario.mensagem, comentariosAnteriores, (str(topico.titulo) + str(topico.descricao)))
-                comentariosAnteriores += comentario.mensagem
-                comentario.inserirRelevanciaTematica(relevancia)
+    def __preProcessarProjeto(projeto:Projeto)-> Projeto:
+        copiaProjeto = deepcopy(projeto)
+        preProcessamento = ControladoraPreProcessamento
+        for i in range(len(projeto.topicos)):
+            topico = copiaProjeto.topicos[i]
+            descricaoPreProcessada = preProcessamento.processar(topico.descricao)[0]
+            topico.setDescricao(descricaoPreProcessada)
+            for j in range(len(topico.listaComentarios)):
+                comentario = topico.listaComentarios[j]
+                comentarioPreProcessado, tags = preProcessamento.processar(comentario.mensagem)
+                projeto.topicos[i].listaComentarios[j].setTags(tags)
+                comentario.setMensagem(comentarioPreProcessado)
+        return copiaProjeto
     
     @staticmethod
-    def __gerarCSVGitHub(projeto, visao,numPagina, arg ):
+    def __calcularRelevanciaTematicaComentariosGitHub(projeto:Projeto,projetoPreProcessado:Projeto):
+        comentariosAnteriores = ''
+        for i in range (len(projetoPreProcessado.topicos)):
+            topico = projetoPreProcessado.topicos[i]
+            for j in range((len(topico.listaComentarios))):
+                comentario = topico.listaComentarios[j] 
+                relevancia = ControladoraCalculoRelevancia.relevanciaTematica(comentario.mensagem, comentariosAnteriores, (str(topico.titulo) + str(topico.descricao)))
+                comentariosAnteriores += comentario.mensagem
+                projeto.topicos[i].listaComentarios[j].inserirRelevanciaTematica(relevancia)
+    
+    @staticmethod
+    def __gerarCSVGitHub(projeto:Projeto, visao,numPagina, arg ):
         if(visao == 4):
             ControladoraExportacaoDados.setVisaoStrategy(visao=VisaoRelevanciaTematicaPorAutor)
         elif(visao == 5):
             ControladoraExportacaoDados.setVisaoStrategy(visao=VisaoRelevanciaTematicaPorData)
-        return ControladoraExportacaoDados.gerarCSV(projeto,visao,numPagina,arg)
+        return ControladoraExportacaoDados.gerarCSVs(projeto,visao,numPagina,arg)
     
     
